a9c63febed7f3b323b194d9a21c6abb68c052912
softcraftsman <jsbrown@tritiumconsulting.com>
2016-03-22 17:34:21 -0400

    Abstracted livefyre and disqus with config.js file. Added authors partial views. Added recent posts partial. Added tag cloud partial. Added about-us and company specific pages.

---
+++ assets/js/livefyre.js
@@ -0,0 +1,25 @@
+﻿
+var livefyre_loaded = false;
+function load_livefyre() {
+    if (config_livefyre_id !== '')
+    {
+        livefyre_loaded = true;
+        var livefyre_id = config_livefyre_id;
+        (function () {
+            var articleId = fyre.conv.load.makeArticleId(null);
+            fyre.conv.load({}, [{
+                el: 'livefyre-comments',
+                network: "livefyre.com",
+                siteId: livefyre_id,
+                articleId: articleId,
+                signed: false,
+                collectionMeta: {
+                    articleId: articleId,
+                    url: fyre.conv.load.makeCollectionUrl(),
+                }
+            }], function () { });
+        }());
+    }
+};
+
+load_livefyre();
