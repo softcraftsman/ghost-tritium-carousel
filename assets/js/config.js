e1a19e8ea56151f4abc2c200a871813b552fdd7e
John Brown <jsbrown@tritiumconsulting.com>
2016-07-13 09:36:09 -0400

    Upgrade to 0.0.4

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+﻿// Enable Google Services
+var google_analytics_key = '';
+var google_search_engine_id = '';
+
+// Enable Disqus
+var config_disqus_shortname = '';
+
+// Enable Livefyre
+var config_livefyre_id = '';
